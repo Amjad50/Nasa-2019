0e25364b108522758443734c2bb63362e951fa45
Amjad Alsharafi <amjadsharafi10@gmail.com>
2019-10-19 21:33:54 +0800

    Edit: changed size and starting point
    - changed the size of the map object
    - added starting point to UM.

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,15 @@
-
+const START_LOC = [3.119993,101.6543625];
+const START_ZOOM = 17;
 
 (function main() {
-    let mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  let mymap = L.map("mapid").setView(START_LOC, START_ZOOM);
 
-    let tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-    });
+  let tileLayer = L.tileLayer(
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+      maxZoom: 19
+    }
+  );
 
-    tileLayer.addTo(mymap);
+  tileLayer.addTo(mymap);
 })();
