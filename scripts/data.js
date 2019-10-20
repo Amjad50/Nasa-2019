5d10115e468609825e21ea3271452c41c3e4fc44
Amjad Alsharafi <amjadsharafi10@gmail.com>
2019-10-20 12:34:32 +0800

    Edit: used custom, data to be used with random
    - if there is data, the map should use it.
    - else, use a random generation.

--- scripts/data.js
+++ scripts/data.js
@@ -1,10 +1,10 @@
 data = {
   points: [
-    ["Cub Creek Reservoir 8-B", [40.2287, -96.9807]],
-    ["Cub Creek Reservoir 8-C", [40.2312, -96.9658]],
-    ["Cub Creek Reservoir 10-C", [40.243, -96.9769]],
-    ["Cub Creek Reservoir 10-A", [40.28, -97.0175]],
-    ["Cub Creek Reservoir 12-C", [40.2791, -97.0476]],
+    ["Cub Creek Reservoir 8-B", [40.2287, -96.9807], [100, 0.5, 8, 0.05, 20, 8]],
+    ["Cub Creek Reservoir 8-C", [40.2312, -96.9658], [90, 2, 15, 0.2, 40, 5]],
+    ["Cub Creek Reservoir 10-C", [40.243, -96.9769], [50, 6, 40, 0.8, 100, 0.5]],
+    ["Cub Creek Reservoir 10-A", [40.28, -97.0175], [30, 10, 80, 2, 200, 4.8]],
+    ["Cub Creek Reservoir 12-C", [40.2791, -97.0476], [5, 28, 120, 4, 300, 4]],
     ["Cub Creek Reservoir 12-D", [40.2821, -97.0763]],
     ["Cub Creek Reservoir G-20", [40.2558, -96.9019]],
     ["Cub Creek Reservoir G-7", [40.2453, -96.9363]],
