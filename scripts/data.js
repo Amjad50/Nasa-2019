bc574ece0f091d7ed52bc6d3ef54f53ac94ede54
Amjad Alsharafi <amjadsharafi10@gmail.com>
2019-10-20 13:06:18 +0800

    Edit: changed the location of some points
    - made it further than other points
    - seems better and more realistic.
    - more manual data.

--- scripts/data.js
+++ scripts/data.js
@@ -4,11 +4,12 @@ data = {
     ["Cub Creek Reservoir 8-C", [40.2312, -96.9658], [90, 2, 15, 0.2, 40, 5]],
     ["Cub Creek Reservoir 10-C", [40.243, -96.9769], [50, 6, 40, 0.8, 100, 0.5]],
     ["Cub Creek Reservoir 10-A", [40.28, -97.0175], [30, 10, 80, 2, 200, 4.8]],
-    ["Cub Creek Reservoir 12-C", [40.2791, -97.0476], [5, 28, 120, 4, 300, 4]],
+    ["Cub Creek Reservoir 12-C", [ 40.19067624876367, -96.80425498686152], [5, 28, 120, 4, 300, 4]],
+    ["Cub Creek Reservoir 12-C", [  40.19303658921225, -96.81873157424553], [30, 10, 80, 2, 200, 4.8]],
     ["Cub Creek Reservoir 12-D", [40.2821, -97.0763]],
-    ["Cub Creek Reservoir G-20", [40.2558, -96.9019]],
+    ["Cub Creek Reservoir G-20", [40.2558, -96.9019], [90, 2, 15, 0.2, 38, 5]],
     ["Cub Creek Reservoir G-7", [40.2453, -96.9363]],
-    ["Big India Creek Reservoir 17-B", [40.183, -96.8968]],
+    ["Big India Creek Reservoir 17-B", [40.183, -96.8968], [90, 2, 15, 0.2, 39, 6]],
     ["Big India Creek Reservoir 16-B", [40.1673, -96.8901]],
   ]
 };
